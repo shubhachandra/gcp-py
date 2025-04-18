718739a8edfb05ebbf1653a0349b008f56313850
shubhachandra <shubam.shubh@gmail.com>
2025-04-18 20:56:00 +0530

    Update Output.tf

--- Output.tf
+++ Output.tf
@@ -1,15 +1,17 @@
-resource "google_compute_subnetwork_iam_member" "core_subnet_access" {
+resource "google_compute_subnetwork_iam_member" "subnet_group_access" {
+  provider = google-beta
+
   for_each = {
     for subnet_link in local.default_networks["core"] : subnet_link => {
+      region = split("/", subnet_link)[3]
+      name   = split("/", subnet_link)[5]
       project = local.default_host_projects["core"]
-      region  = split("/", subnet_link)[4]
-      name    = split("/", subnet_link)[6]
     }
   }
 
-  project = each.value.project
-  region  = each.value.region
+  project    = each.value.project
+  region     = each.value.region
   subnetwork = each.value.name
-  role    = "roles/compute.networkUser"
-  member  = local.subnet_access_groups["core"]
+  role       = "roles/compute.networkUser"
+  member     = local.subnet_access_groups["core"]
 }
