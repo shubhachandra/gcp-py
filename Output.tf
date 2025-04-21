c80301725ee86faaed3f9c7f523034b114ba4890
shubhachandra <shubam.shubh@gmail.com>
2025-04-21 22:46:09 +0530

    Update Output.tf

--- Output.tf
+++ Output.tf
@@ -13,3 +13,15 @@ resource "google_cloud_identity_group_membership" "example_member" {
     name = "MEMBER"
   }
 }
+
+resource "google_cloud_identity_group_membership" "example" {
+  group = "groups/your-group-id" # Replace with your group resource name
+
+  preferred_member_key {
+    id = format("serviceAccount:%s@cloudservices.gserviceaccount.com", local.service_project_number)
+  }
+
+  roles {
+    name = "MEMBER"
+  }
+}
