be11a99d1bc8e874d45ce0c903da8494a218db96
shubhachandra <shubam.shubh@gmail.com>
2025-04-22 17:03:13 +0530

    Update Output.tf

--- Output.tf
+++ Output.tf
@@ -1,3 +1,4 @@
+ https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=32555940559.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A8085%2F&scope=openid+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcloud-platform+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fappengine.admin+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fsqlservice.login+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcompute+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Faccounts.reauth&state=3pOEOu0UT4oWaiv2zZcUkNLGVHJUCD&access_type=offline&code_challenge=KAtXaQGLPJgARVzloF2xrf8c2of2euut-mZ-yoc4Vp8&code_challenge_method=S256
 data "google_cloud_identity_group" "subnetwork_group" {
   group_email = var.group_email
 }
