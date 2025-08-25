73afee24e947c93efceec4ad3e85d9fa718261a9
shubhachandra <shubam.shubh@gmail.com>
2025-08-25 18:42:42 +0530

    Update Output.tf

--- Output.tf
+++ Output.tf
@@ -1,36 +1,32 @@
-resource "google_cloud_identity_group_membership" "sa_membership" {
-  group = var.group_id
-
-  preferred_member_key {
-    id = var.service_account_email
-  }
-
-  roles {
-    name = "MEMBER"
-  }
-}
-
-
-variable "service_account_email" {
-  description = "Service account email to add to the group"
-  type        = string
-}
-
-variable "group_id" {
-  description = "Group ID in format 'groups/{group_id}' (not group email)"
-  type        = string
-}
-
-output "membership_resource_name" {
-  value       = google_cloud_identity_group_membership.sa_membership.name
-  description = "Full resource name of the membership"
-}
 terraform {
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = ">= 4.58.0"
+    bluecat = {
+      source  = "bluecatlabs/bluecat"
+      version = ">= 0.1.0"
     }
   }
-  required_version = ">= 1.3"
 }
+
+provider "bluecat" {
+  username    = var.bluecat_username      # Service account or user name
+  password    = var.bluecat_password      # Sensitive, keep in Vault or env vars
+  server      = var.bluecat_server        # IP or hostname of BAM/Gateway
+  port        = 443                       # Typically 443 (HTTPS) or 80 (HTTP)
+  transport   = "https"                   # "https" or "http"
+  api_version = "v1"                      # Example: "v1" or the version supported by your Gateway
+}
+variable "bluecat_username" {
+  description = "BlueCat service account username"
+}
+
+variable "bluecat_password" {
+  description = "BlueCat service account password"
+  sensitive   = true
+}
+
+variable "bluecat_server" {
+  description = "BlueCat BAM or Gateway hostname/IP"
+}
+export TF_VAR_bluecat_username=svc_terraform
+export TF_VAR_bluecat_password='********'
+export TF_VAR_bluecat_server='100.120.136.196'
